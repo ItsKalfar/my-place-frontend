d53e07f4f732182ddb9f6ce4435ab39840dd1b6d
ItsKalfar <gundsagar02@gmail.com>
2023-03-04 11:27:36 +0530

    Added toekn smart contract

--- backend/hardhat-helper.config.js
+++ backend/hardhat-helper.config.js
@@ -4,14 +4,26 @@ const networkConfig = {
   default: {
     name: "hardhat",
     marketPlaceAddress: process.env.MYPLACE_CONTRACT_ADDRESS,
+    tokenName: "ArsyToken",
+    symbol: "ARSY",
+    decimals: "18",
+    totalSupply: ethers.utils.parseEther("10000000"),
   },
   31337: {
     name: "localhost",
     marketPlaceAddress: process.env.MYPLACE_CONTRACT_ADDRESS,
+    tokenName: "ArsyToken",
+    symbol: "ARSY",
+    decimals: "18",
+    totalSupply: ethers.utils.parseEther("10000000"),
   },
   5: {
     name: "goerli",
     marketPlaceAddress: process.env.MYPLACE_CONTRACT_ADDRESS,
+    tokenName: "ArsyToken",
+    symbol: "ARSY",
+    decimals: "18",
+    totalSupply: ethers.utils.parseEther("10000000"),
   },
 };
 
